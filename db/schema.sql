b16db6f67c1db1da3abf27b6fa8409f8609c2371
magentareds <thething1845@yahoo.com>
2017-04-29 19:59:54 -0500

    Updated table schema.

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ USE burgers_db;
 CREATE TABLE burgers
 (
   id INTEGER NOT NULL AUTO_INCREMENT,
-  burger_name VARCHAR(255),
+  burger_name VARCHAR(255) NOT NULL,
   devoured BOOLEAN DEFAULT false,
-  date TIMESTAMP,
+  date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY(id)
 );
