b91b448e4e99102aa5139f760a99e9053342199e
magentareds <thething1845@yahoo.com>
2017-04-29 20:28:12 -0500

    Updated burger submitting to not accept the empty string.

--- public/js/view.js
+++ public/js/view.js
@@ -1,12 +1,15 @@
 $(document).ready(function(){
   $("#submit-burger-form").on("submit", function(event){
     event.preventDefault();
-    var myBurger = {
-      burger_name: $("#burger-name-input").val().trim()
-    };
+    var burgerName = $("#burger-name-input").val().trim();
+    if(burgerName!==""){
+      var myBurger = {
+        burger_name: burgerName
+      };
 
-    $.post("/", myBurger, function(){
-      window.location.reload();
-    });
+      $.post("/", myBurger, function(){
+        window.location.reload();
+      });
+    }
   });
 });
